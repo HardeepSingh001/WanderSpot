eba13450df8e3aea7740856bfb272ddd53a59914
HardeepSingh001 <knightc0der001@gmail.com>
2023-07-09 20:08:24 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const express=require("express")
+const app=express()
+const path=require("path")
+const Spot=require('./models/spot')
+const methodOverride=require('method-override')
+const ejsMate=require('ejs-mate')
+app.set('view engine','ejs')
+app.set("views",path.join(__dirname,"views"))
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
+app.engine('ejs',ejsMate)
+
+
+
+const mongoose=require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/wander-spot',{
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db=mongoose.connection;
+db.on("error",console.error.bind(console,"connection error:"));
+db.once("open",()=>{
+    console.log("Database connected");
+});
+
+app.get('/home',(req,res)=>{
+    res.render("home")
+})
+
+app.get("/spots",async(req,res)=>{
+    const spots=await Spot.find({});
+    res.render('spots/index',{spots})
+})
+
+app.get('/spots/new',(req,res)=>{
+    res.render('spots/new')
+})
+
+app.post('/spots',async(req,res)=>{
+    const spot=new Spot(req.body.spot);
+    await spot.save();
+
+    res.redirect(`spots/${spot._id}`)
+
+})
+
+app.get('/spots/:id',async(req,res)=>{
+    const spot=await Spot.findById(req.params.id)
+    res.render('spots/show',{spot})
+})
+
+app.get('/spots/:id/edit',async(req,res)=>{
+    const spot=await Spot.findById(req.params.id)
+    res.render('spots/edit',{spot})
+})
+
+app.put('/spots/:id',async(req,res)=>{
+    const spot=await Spot.findByIdAndUpdate(req.params.id,{...req.body.spot})
+
+    res.redirect(`spots/${spot._id}`)
+})
+
+app.delete('/spots/:id',async(req,res)=>{
+    await Spot.findByIdAndDelete(req.params.id)
+    res.redirect('/spots')
+})
+
+app.listen(3000,()=>{
+    console.log("Listening on port 3000..")
+})
---
+++ seeds/index.js
@@ -0,0 +1,35 @@
+
+const Spot=require('../models/spot')
+const cities=require('./cities')
+const {places,descriptors}=require('./seedHelpers')
+
+const mongoose=require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/wander-spot',{
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db=mongoose.connection;
+db.on("error",console.error.bind(console,"connection error:"));
+db.once("open",()=>{
+    console.log("Database connected");
+});
+
+const sample= array =>array[Math.floor(Math.random()*array.length)];
+
+const seedDB=async()=>{
+    await Spot.deleteMany({});
+    for(let i=0;i<50;i++){
+        const random200=Math.floor(Math.random()*200);
+        const s=new Spot({
+            location: `${cities[random200].City}, ${cities[random200].State}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await s.save();
+    }
+}
+
+seedDB().then(()=>{
+    mongoose.connection.close()
+});
