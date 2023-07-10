d911b9e599647dc076b345cae916e094a25c1cc6
HardeepSingh001 <knightc0der001@gmail.com>
2023-07-10 19:13:58 +0530

    Added partials and Layouts, did basic styling and used bootstrap and finally updated model class

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB=async()=>{
     await Spot.deleteMany({});
     for(let i=0;i<50;i++){
         const random200=Math.floor(Math.random()*200);
+        const price=Math.floor(Math.random()*1000)+10;
         const s=new Spot({
             location: `${cities[random200].City}, ${cities[random200].State}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro enim dolore qui fugit est minus, voluptas nesciunt, ducimus quo consequuntur perferendis. Id consectetur impedit eius dignissimos tempore qui cupiditate voluptatum?',
+            price: price
         })
         await s.save();
     }
